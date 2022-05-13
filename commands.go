72eaeb9ee861bdd49d314b9d6b613f16e8b629b2
sinku <sininoir@gmail.com>
2022-05-13 19:33:13 +0430

    Refactor commands

--- commands.go
+++ commands.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"strings"
+	"os"
 )
 
 const (
@@ -11,19 +10,6 @@ const (
 )
 
 type CommandHandler func([]string, *Player)
-
-var ActiveCommandset map[int]CommandHandler
-var ActiveCommandsetNames map[int]string
-
-var DefendingMoveNames = map[int]string{
-	1: "Block",
-	2: "Dodge",
-}
-
-var DefendingMoves = map[int]CommandHandler{
-	1: HandleBlock,
-	2: HandleDodge,
-}
 
 func HandleBlock(args []string, player *Player) {
 	fmt.Println("handling block")
@@ -33,32 +19,19 @@ func HandleDodge(args []string, player *Player) {
 	fmt.Println("handling dodge")
 }
 
-func getCommandRowLenght() int {
-	maxLen := -1
-	for _, val := range ActiveCommandsetNames {
-		x := len(val)
-		if x > maxLen {
-			maxLen = x
-		}
-	}
-	return 3 + SpacesInBetween + maxLen + 3
+func HandleFight(args []string, player *Player) {
+	fmt.Println("handling fight")
 }
 
-func PrintAvailableCommands() {
-	rowLen := getCommandRowLenght()
-	fmt.Println(strings.Repeat("-", rowLen))
-	keys := make([]int, 0)
-	for k := range ActiveCommandsetNames {
-		keys = append(keys, k)
-	}
-	sort.Ints(keys)
-	for _, key := range keys {
-		fmt.Printf("|%2d.%v%v |\n", key, strings.Repeat(" ", SpacesInBetween), ActiveCommandsetNames[key])
-	}
-	fmt.Println(strings.Repeat("-", rowLen))
+func HandleLevelup(args []string, player *Player) {
+	fmt.Println("handling fight")
 }
 
-func SetActiveCommandSet(activeCommandset map[int]CommandHandler, activeCommandsetNames map[int]string) {
+func HandleExit(args []string, player *Player) {
+	fmt.Println("See you back in the ring...")
+	os.Exit(0)
+}
+
+func SetActiveCommandSet(activeCommandset Commandset) {
 	ActiveCommandset = activeCommandset
-	ActiveCommandsetNames = activeCommandsetNames
 }
