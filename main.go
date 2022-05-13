faec30b54604db2ba3aa8463af039f4f3fe836d7
sinku <sininoir@gmail.com>
2022-05-13 19:05:44 +0430

    Clean main

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print(">")
-	name, _ := reader.ReadString('\n')
-	name = strings.Replace(name, "\n", "", -1)
-	p := MakePlayer(name)
+	p := MakePlayer(ReadLine())
 	fmt.Println(p.getInfo())
+	SetActiveCommandSet(DefendingMoves, DefendingMoveNames)
+	run(p)
 }
