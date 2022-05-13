8ff83f4b08f28a9d2f992123ec914d1d481b32d8
sinku <sininoir@gmail.com>
2022-05-13 19:33:47 +0430

    Refactor functions into file

--- console.go
+++ console.go
@@ -4,13 +4,40 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
 func ReadLine() string {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print(">")
+	fmt.Print("> ")
 	name, _ := reader.ReadString('\n')
 	name = strings.Replace(name, "\n", "", -1)
 	return name
+}
+
+func getCommandRowLenght() (int, int) {
+	maxLen := -1
+	for _, val := range ActiveCommandset.Names {
+		x := len(val)
+		if x > maxLen {
+			maxLen = x
+		}
+	}
+	return 3 + SpacesInBetween + maxLen + 3, maxLen
+}
+
+func PrintAvailableCommands() {
+	rowLen, maxLen := getCommandRowLenght()
+	fmt.Println(strings.Repeat("-", rowLen))
+	keys := make([]int, 0)
+	for k := range ActiveCommandset.Names {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, key := range keys {
+		commandName := ActiveCommandset.Names[key]
+		fmt.Printf("|%2d.%v%v%v |\n", key, strings.Repeat(" ", SpacesInBetween), commandName, strings.Repeat(" ", maxLen-len(commandName)))
+	}
+	fmt.Println(strings.Repeat("-", rowLen))
 }
