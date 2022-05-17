efff79b9b0f5750b7cfcd71c49ec0f4c0f4ff18a
sinku <sininoir@gmail.com>
2022-05-18 01:40:07 +0430

    Add base enemy interface

---
+++ enemy.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/fatih/camelcase"
+)
+
+const (
+	BackstreetFighterName = "Backstreet Fighter"
+)
+
+var EnemyRegistery = make(map[string]reflect.Type)
+
+type Enemy interface {
+	ChooseMove()
+}
+
+type BackstreetFighter struct {
+	Name   string
+	Hp     float64
+	Damage float64
+	Lvl    int
+}
+
+func (bf BackstreetFighter) ChooseMove() {
+	fmt.Println("choosing move")
+}
+
+func InitEnemyRegistery() {
+	myTypes := []interface{}{BackstreetFighter{}}
+	for _, v := range myTypes {
+		EnemyRegistery[reflect.TypeOf(v).Name()] = reflect.TypeOf(v)
+	}
+}
+
+func MakeEnemy(enemyName string) Enemy {
+	v := reflect.New(EnemyRegistery[enemyName]).Elem()
+	v.FieldByName("Name").SetString(strings.Join(camelcase.Split(enemyName)[:], " "))
+	v.FieldByName("Lvl").SetInt(10)
+
+	enemy := v.Interface().(Enemy)
+
+	// enemy(strings.Join(camelcase.Split(enemyName)[:], " "))
+
+	// TODO: Maybe fill in fields here if necessary
+	return enemy
+}
